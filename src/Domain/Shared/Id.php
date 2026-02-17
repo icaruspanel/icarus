16f38806d6e7a60834b09ee6f6ad1e2a07d0e196
Ollie Read <code@ollieread.com>
2026-02-17 20:09:29 +0000

    feat(core:shared): Implement Stringable on Id base class

--- src/Domain/Shared/Id.php
+++ src/Domain/Shared/Id.php
@@ -3,12 +3,13 @@ declare(strict_types=1);
 
 namespace Icarus\Domain\Shared;
 
+use Stringable;
 use Symfony\Component\Uid\Ulid;
 
 /**
  *
  */
-abstract readonly class Id
+abstract readonly class Id implements Stringable
 {
     /**
      * @return static
@@ -31,5 +32,17 @@ abstract readonly class Id
         assert(Ulid::isValid($id), 'Invalid ULID format');
 
         $this->id = $id;
+    }
+
+    /**
+     * Magic method {@see https://www.php.net/manual/en/language.oop5.magic.php#object.tostring}
+     * allows a class to decide how it will react when it is treated like a string.
+     *
+     * @return string Returns string representation of the object that
+     * implements this interface (and/or "__toString" magic method).
+     */
+    public function __toString(): string
+    {
+        return $this->id;
     }
 }
