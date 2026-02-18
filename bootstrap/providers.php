d33a2c14db561bbc2a537ed1e5c22b5e0071dc78
Ollie Read <code@ollieread.com>
2026-02-18 12:56:56 +0000

    feat: register domain service providers
    Add SharedServiceProvider, UserDomainServiceProvider, and
    AuthTokenDomainServiceProvider to the providers bootstrap.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,11 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+
+    // Icarus Providers
+    Icarus\Infrastructure\Shared\SharedServiceProvider::class,
+
+    // Icarus Domain Providers
+    Icarus\Infrastructure\User\UserDomainServiceProvider::class,
+    Icarus\Infrastructure\AuthToken\AuthTokenDomainServiceProvider::class,
 ];
